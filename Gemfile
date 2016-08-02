2184f86c910e2836b550d8826a4e2401eb895d37
Jesse Griffin <jag3773@gmail.com>
2016-08-02 17:01:56 -0600

    Added Markdown lint check to cibuild

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem "jekyll"
 gem "html-proofer"
+gem "mdl"
 gem 'jekyll-sitemap'
 gem 'kramdown'
 gem 'jekyll-watch'
--- cibuild.sh
+++ cibuild.sh
@@ -4,6 +4,9 @@ set -e # halt script on error
 # build site
 bundle exec jekyll build
 
+# Run Markdown Proofer against built site
+bundle exec mdl ./_site
+
 # Run HTML Proofer against built site
 bundle exec htmlproofer ./_site --disable-external --assume-extension --check-html --file-ignore ./_site/templates/reveal.html
 
