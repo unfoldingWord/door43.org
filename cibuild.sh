38ab9b4b5133e63d4c5eb287a78999bb792ce8b8
Jesse Griffin <jag3773@gmail.com>
2016-08-02 17:23:09 -0600

    Pointed mdl at pages directory

--- cibuild.sh
+++ cibuild.sh
@@ -5,7 +5,7 @@ set -e # halt script on error
 bundle exec jekyll build
 
 # Run Markdown Proofer against built site
-bundle exec mdl ./_site
+bundle exec mdl ./pages
 
 # Run HTML Proofer against built site
 bundle exec htmlproofer ./_site --disable-external --assume-extension --check-html --file-ignore ./_site/templates/reveal.html
