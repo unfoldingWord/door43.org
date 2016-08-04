b35b5d75768ff741ef8407ddf15e7e415718d2cc
Jesse Griffin <jag3773@gmail.com>
2016-08-04 17:11:14 -0600

    Added config for travis to push to test S3 bucket

---
+++ s3_dev_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2016 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@unfoldingword.org>
+
+SOURCE="_site/"
+BKT="s3://test-door43.org/"
+EXCLUDES="s3_excludes"
+
+s3cmd -c s3cfg-dev sync --rr -M -F \
+    --no-mime-magic --delete-removed \
+    --exclude-from "$EXCLUDES" \
+    --add-header="Cache-Control:max-age=600" \
+    "$SOURCE" "$BKT"
