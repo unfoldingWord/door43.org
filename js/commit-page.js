b60cc7629dfe90d2badd6d716d19fb2bfd8cfd4f
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-09-26 10:42:34 -0600

    Removed checking for existing pages

--- js/commit-page.js
+++ js/commit-page.js
@@ -3,29 +3,17 @@
  */
 
 $(document).ready(function(){
-  var url_parts = window.location.href.split('?')[0].split('/');
-  var parent_url = url_parts.slice(0,-2).join('/');
-  var myCommitId = url_parts.slice(-1);
-
-  var request;
-  if(window.XMLHttpRequest)
-    request = new XMLHttpRequest();
-  else
-    request = new ActiveXObject("Microsoft.XMLHTTP");
-
   $.getJSON( "build_log.json", function(myLog) {
+    var myCommitId = myLog.commit_id.substring(0, 10);
+    $('#last-updated').html("Updated "+timeSince(new Date(myLog.created_at))+" ago");
+    $('#build-status-icon i').attr("class", "fa "+getStatusIcon(myLog.status));
+
     console.log("Building sidebar for "+myCommitId);
 
-    $('#left-sidebar').html('<div><h1>Revisions</h1><table width="100%" id="revisions"></table></div>');
+    $('#left-sidebar #revisions').empty()
 
     $.getJSON("../project.json", function (project) {
       $.each(project.commits.reverse(), function (index, commit) {
-        request.open('GET', parent_url+"/"+commit.id+"/01.html", false);
-        request.send();
-        if (request.status === 404) {
-           return true;
-        }
-
         date = new Date(commit.created_at);
         var options = {
           year: "numeric",
@@ -39,11 +27,6 @@ $(document).ready(function(){
 
         statusIcon = getStatusIcon(commit.status)
 
-        if(commit.id == myCommitId){
-          $('#last-updated').html("Updated "+timeSince(date)+" ago");
-          $('#build-status-icon i').attr("class", "fa "+statusIcon);
-        }
-
         var html = '<tr><td>';
         if(commit.id == myCommitId){
           html += '<b>'+dateStr+'</b>';
@@ -51,7 +34,7 @@ $(document).ready(function(){
         else
           html += '<a href="../' + commit.id + '/index.html">' + dateStr + '</a>';
         html += '</td><td><i class="fa '+statusIcon+'"></i></td></tr>';
-        $('#revisions').append(html);
+        $('#left-sidebar #revisions').append(html);
       }); // End each
     })
     .done(function () {
