6b58ef6d2f17a520ce237741260ada9224fb61c7
Bruce McLean <mcleanb@photo-nomad.net>
2017-02-04 13:57:49 -0500

    Left fabulous fonts spinner in place as default.  In the case of status success/warning/error then spinner is replaced with img icon.

--- js/general-tools.js
+++ js/general-tools.js
@@ -28,14 +28,74 @@ function timeSince(date) {
     return Math.floor(seconds) + " seconds";
 }
 
-function getStatusIcon(status) {
-    switch(status){
-        case 'requested':
-        case 'started': return 'fa-spinner fa-spin';
-        case 'success': return 'fa-check-circle';
-        case 'warnings': return 'fa-exclamation-circle';
-        case 'critical':
-        case 'failed':
-        default: return 'fa-times-circle';
+eConvStatus = {
+    IN_PROGRESS : 1,
+    SUCCESS : 2,
+    WARNING : 3,
+    ERROR: 4
+}
+
+var faSpinnerClass = 'fa-spinner fa-spin';
+const StatusImagesUrl = "https://cdn.door43.org/assets/img/icons/";
+
+function setOverallConversionStatus(status) {
+    var iconType = getDisplayIconType(status);
+    if (iconType != eConvStatus.IN_PROGRESS) {
+        iconHtml = getConversionStatusIconHtml(iconType, status, true, true);
+        $('#build-status-icon').html(iconHtml); // replace default spinner
     }
 }
+
+function getCommitConversionStatusIcon(status) {
+    var iconType = getDisplayIconType(status);
+    var html = getConversionStatusIconHtml(iconType, status, false, false);
+    return html;
+}
+
+function getDisplayIconType(status) {
+    switch(status){
+        case 'requested':
+        case 'started': return eConvStatus.IN_PROGRESS;
+        case 'success': return eConvStatus.SUCCESS;
+        case 'warnings': return eConvStatus.WARNING;
+        case 'critical':
+        case 'failed':
+        default: return eConvStatus.ERROR;
+    }
+}
+
+function getConversionStatusIconHtml(iconType, title, longWidth, largeSize) {
+    var url = null;
+    if(iconType == eConvStatus.IN_PROGRESS) {
+        return '<i class="fa ' + faSpinnerClass + '" title="' + title + '"></i>';
+    }
+
+    url = getNewStatusIcon(status, longWidth,largeSize);
+    if(url) {
+        var html = '<img src="' + url + '" alt="'+title+'" >';
+        return html;
+    }
+
+    return "";
+}
+
+function getNewStatusIcon(status, longWidth,largeSize) {
+    var suffix = largeSize ? "-large.png" : "-small.png";
+    var middle = longWidth ? "-long" : "-short";
+    switch(status){
+
+        case eConvStatus.SUCCESS:
+            return buildImageUrl('success', middle, suffix);
+        case eConvStatus.WARNING:
+            return buildImageUrl('warning', middle, suffix);
+        case eConvStatus.ERROR:
+        default:
+            return buildImageUrl('error', middle, suffix);
+    }
+}
+
+function buildImageUrl(prefix, middle, suffix) {
+    var path = StatusImagesUrl + prefix + middle + suffix;
+    return path;
+}
+
