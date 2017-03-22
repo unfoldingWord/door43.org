72b003d5d58e1562041dd63cc1612b7dab167f40
Jesse Griffin <jag3773@gmail.com>
2017-03-22 16:09:26 -0600

    testing deploy via travis

--- s3_prod_push.sh
+++ s3_prod_push.sh
@@ -14,8 +14,11 @@ EXCLUDES="s3_excludes"
 
 openssl aes-256-cbc -K $encrypted_e2db0eb08244_key -iv $encrypted_e2db0eb08244_iv -in secrets.tar.enc -out secrets.tar -d
 tar xvf secrets.tar
+
+which s3cmd
 s3cmd -c s3cfg-prod sync -M -F \
     --no-mime-magic --delete-removed \
     --exclude-from "$EXCLUDES" \
     --add-header="Cache-Control:max-age=600" \
     "$SOURCE" "$BKT"
+    
