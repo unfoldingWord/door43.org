4307fa47aaa6554bc36e6b63837bc67c2cf11502
Jesse Griffin <jag3773@gmail.com>
2017-03-22 17:00:32 -0600

    testing deploy

--- s3_prod_push.sh
+++ s3_prod_push.sh
@@ -15,6 +15,8 @@ EXCLUDES="s3_excludes"
 openssl aes-256-cbc -K $encrypted_e2db0eb08244_key -iv $encrypted_e2db0eb08244_iv -in secrets.tar.enc -out secrets.tar -d
 tar xvf secrets.tar
 
+grep "^access_key" s3cfg-prod | cut -f 2 -d 7
+
 s3cmd -c s3cfg-prod sync -M -F \
     --no-mime-magic --delete-removed \
     --exclude-from "$EXCLUDES" \
