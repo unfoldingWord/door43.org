237b039e60bb57c825c97f537bd0ba9a883f9ea0
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2017-05-16 19:56:32 -0400

    Reformat js file for consistency

--- js/project-page.js
+++ js/project-page.js
@@ -2,40 +2,42 @@
  * Javascript for project commit pages to update the status and build the left sidebar
  */
 
-$(document).ready(function(){
-  $('#starred-icon').click(function(){
-    if($(this).hasClass('starred')){
-        $(this).removeClass('starred');
-        $(this).find('i').removeClass('fa-star').addClass('fa-star-o');
+$(document).ready(function () {
+  $('#starred-icon').click(function () {
+    if ($(this).hasClass('starred')) {
+      $(this).removeClass('starred');
+      $(this).find('i').removeClass('fa-star').addClass('fa-star-o');
     }
     else {
-        $(this).addClass('starred');
-        $(this).find('i').removeClass('fa-star-o').addClass('fa-star');
+      $(this).addClass('starred');
+      $(this).find('i').removeClass('fa-star-o').addClass('fa-star');
     }
   });
 
   var filename = window.location.href.split('?')[0].split('/').pop();
-  $('#left-sidebar #page-nav option[value="'+filename+'"]').attr('selected','selected');
+  $('#left-sidebar').find('#page-nav option[value="' + filename + '"]').attr('selected', 'selected');
 
-  $.getJSON( "build_log.json", function(myLog) {
+  $.getJSON("build_log.json", function (myLog) {
     var myCommitId = myLog.commit_id.substring(0, 10);
-    $('#last-updated').html("Updated "+timeSince(new Date(myLog.created_at))+" ago");
-    $('#build-status-icon i').attr("class", "fa "+ faSpinnerClass); // default to spinner
+    $('#last-updated').html("Updated " + timeSince(new Date(myLog.created_at)) + " ago");
+
+    var $buildStatusIcon = $('#build-status-icon');
+    $buildStatusIcon.find('i').attr("class", "fa " + faSpinnerClass); // default to spinner
     setOverallConversionStatus(myLog.status);
-    if(myLog.errors.length > 0)
-        $('#build-status-icon').attr("title", myLog.errors.join("\n"));
-    else if(myLog.warnings.length > 0)
-        $('#build-status-icon').attr("title", myLog.warnings.join("\n"));
-    else if(myLog.message)
-        $('#build-status-icon').attr("title", myLog.message);
+    if (myLog.errors.length > 0)
+      $buildStatusIcon.attr("title", myLog.errors.join("\n"));
+    else if (myLog.warnings.length > 0)
+      $buildStatusIcon.attr("title", myLog.warnings.join("\n"));
+    else if (myLog.message)
+      $buildStatusIcon.attr("title", myLog.message);
 
-    console.log("Building sidebar for "+myCommitId);
+    console.log("Building sidebar for " + myCommitId);
 
-    $('#left-sidebar #revisions').empty();
+    $('#left-sidebar').find('#revisions').empty();
 
     $.getJSON("../project.json", function (project) {
       $.each(project.commits.reverse(), function (index, commit) {
-        date = new Date(commit.created_at);
+        var date = new Date(commit.created_at);
         var options = {
           year: "numeric",
           month: "short",
@@ -44,11 +46,11 @@ $(document).ready(function(){
           minute: "numeric",
           timeZone: "UTC"
         };
-        dateStr = date.toLocaleString("en-US", options);
+        var dateStr = date.toLocaleString("en-US", options);
 
         var html = '<tr><td>';
-        if(commit.id == myCommitId){
-          html += '<b>'+dateStr+'</b>';
+        if (commit.id === myCommitId) {
+          html += '<b>' + dateStr + '</b>';
         }
         else {
           html += '<a href="../' + commit.id + '/index.html">' + dateStr + '</a>';
@@ -56,26 +58,26 @@ $(document).ready(function(){
 
         var iconHtml = getCommitConversionStatusIcon(commit.status);
         html += '</td><td>' + iconHtml + '</td></tr>';
-        $('#left-sidebar #revisions').append(html);
+        $('#left-sidebar').find('#revisions').append(html);
       }); // End each
     })
+      .done(function () {
+        console.log("processed project.json");
+      })
+      .fail(function () {
+        console.log("error reading project.json");
+      }); // End getJSON
+  })
     .done(function () {
-      console.log("processed project.json");
+      console.log("processed my own build_log.json");
     })
     .fail(function () {
-      console.log("error reading project.json");
+      console.log("error reading my own build_log.json");
     }); // End getJSON
-  })
-  .done(function () {
-    console.log("processed my own build_log.json");
-  })
-  .fail(function () {
-    console.log("error reading my own build_log.json");
-  }); // End getJSON
 
   // pin the header below the menu rather than scroll out of view
   var $pinned = $('#pinned-header');
-  var $menu = $($('nav.navbar-fixed-top')[0]);
+  // var $menu = $($('nav.navbar-fixed-top')[0]);
   var margin_top = parseInt($pinned.css('margin-top'));
 
   $(document).on('scroll', function () {
@@ -99,43 +101,44 @@ $(document).ready(function(){
       top: navHeight
     }
   });
-  $('body').scrollspy({ target: '#right-sidebar-nav', offset: navHeight});
+  $('body').scrollspy({target: '#right-sidebar-nav', offset: navHeight});
   /* smooth scrolling to sections with room for navbar */
-  $("#right-sidebar-nav li a[href^='#']").on('click', function (e) {
-        // prevent default anchor click behavior
-        e.preventDefault();
-        // store hash
-        var hash = this.hash;
-        // animate
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top - navHeight
-        }, 300, function () {
-            // when done, add hash to url
-            // (default click behaviour)
-            window.location.hash = hash;
-        });
+  var $rightSidebarNav = $("#right-sidebar-nav");
+  $rightSidebarNav.find("li a[href^='#']").on('click', function (e) {
+    // prevent default anchor click behavior
+    e.preventDefault();
+    // store hash
+    var hash = this.hash;
+    // animate
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - navHeight
+    }, 300, function () {
+      // when done, add hash to url
+      // (default click behaviour)
+      window.location.hash = hash;
+    });
   });
 
   /* Scroll to current section if URL has hash */
-  if(window.location.hash){
-    $("#right-sidebar-nav li a[href='#"+window.location.hash+"']").trigger('click');
+  if (window.location.hash) {
+    $rightSidebarNav.find("li a[href='#" + window.location.hash + "']").trigger('click');
   }
 
-  $(window).on('scroll resize', function(){
-    $('#right-sidebar-nav #sidebar-nav').css('bottom', getVisibleHeight('footer'));
+  $(window).on('scroll resize', function () {
+    $rightSidebarNav('#sidebar-nav').css('bottom', getVisibleHeight('footer'));
   });
 });
 
 function getVisibleHeight(selector) {
-    var $el = $(selector),
-        scrollTop = $(this).scrollTop(),
-        scrollBot = scrollTop + $(this).height(),
-        elTop = $el.offset().top,
-        elBottom = elTop + $el.outerHeight(),
-        visibleTop = elTop < scrollTop ? scrollTop : elTop,
-        visibleBottom = elBottom > scrollBot ? scrollBot : elBottom;
-    if((visibleBottom - visibleTop) > 0)
-      return visibleBottom - visibleTop;
-    else
-      return 0;
+  var $el = $(selector),
+    scrollTop = $(this).scrollTop(),
+    scrollBot = scrollTop + $(this).height(),
+    elTop = $el.offset().top,
+    elBottom = elTop + $el.outerHeight(),
+    visibleTop = elTop < scrollTop ? scrollTop : elTop,
+    visibleBottom = elBottom > scrollBot ? scrollBot : elBottom;
+  if ((visibleBottom - visibleTop) > 0)
+    return visibleBottom - visibleTop;
+  else
+    return 0;
 }
