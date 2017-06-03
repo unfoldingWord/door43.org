bdfbe69cfd761eb7c725baf7d6560ff5729944d6
Bruce McLean <mcleanb@photo-nomad.net>
2017-06-03 07:21:00 -0400

    Added coverage for the other js files.

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,9 @@ module.exports = function(config) {
             // source files, that you wanna generate coverage for
             // do not include tests or libraries
             // (these files will be instrumented by Istanbul)
-            './js/general-tools.js': 'coverage'
+            './js/general-tools.js': 'coverage',
+            './js/project-page.js': 'coverage',
+            './js/application.js': 'coverage'
         },
 
         // optionally, configure the reporter
