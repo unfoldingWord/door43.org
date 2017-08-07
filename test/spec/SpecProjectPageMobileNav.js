391e32d4f308121ca31ddc54b525ef80a4451e79
Richard Mahn <rich.mahn@unfoldingword.org>
2017-08-07 10:54:31 -0600

    More tests for mobile

--- test/spec/SpecProjectPageMobileNav.js
+++ test/spec/SpecProjectPageMobileNav.js
@@ -13,6 +13,24 @@ describe('Test Mobile Nav Setup and TearDown', function () {
             expect($('#content-header #mobile-content-nav-toggle').length).toEqual(1);
             expect($('#content-header #mobile-content-nav').length).toEqual(1);
             expect($('#content-body').length).toEqual(1);
+
+            teardownMobileContentNavigation();
+            expect($('#content-header').length).toEqual(0);
+            expect($('#content-header #mobile-content-nav-toggle').length).toEqual(0);
+            expect($('#content-header #mobile-content-nav').length).toEqual(0);
+            expect($('#content-body').length).toEqual(0);
+
+            // Doing it a second time should end up with same results as first time
+            setupMobileContentNavigation();
+            expect($('#content-header').length).toEqual(1);
+            expect($('#content-header #mobile-content-nav-toggle').length).toEqual(1);
+            expect($('#content-header #mobile-content-nav').length).toEqual(1);
+            expect($('#content-body').length).toEqual(1);
+
+            // Not sure how to test as they use slideUp/SlideDown which causes being shown/hidden to happen async
+            openMobileContentNav();
+            closeMobileContentNav();
+            toggleMobileContentNav();
         });
     });
 });
