0046d906f579b5ffbdc97a7f2bcf822e1adb04f3
Bruce McLean <mcleanb@photo-nomad.net>
2017-08-09 17:06:15 -0400

    i18n.html & i18n.js - remove page views from language page

--- js/i18n.js
+++ js/i18n.js
@@ -10,7 +10,7 @@ function onReady() {
     setupLanguageSelector();
     setupSearchFieldFromUrl();
     searchProjects();
-    setLanguagePageViews($('#num-of-views'),window.location.href,1);
+    setLanguagePageViews(null,window.location.href,1);
     pageLoaded = true;
 
     $('#search-button').on('click', function () {
